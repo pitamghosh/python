1660baf74c488e62d0245f5bba048d6169ef7cc7
Pitam Ghosh <164029814+pitamghosh@users.noreply.github.com>
2024-08-07 01:54:45 +0530

    Add files via upload

---
+++ college_DSA/array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a[4];
+    a[0]=25;
+    a[1]=50;
+    a[2]=75;
+    a[3]=100;
+    printf("%d\n %d\n %d\n %d\n",a[0,1,2,3]);
+   // printf("%d\n %d\n %d\n %d\n",a[0],a[1],a[2],a[3]);
+    return 0;
+}
